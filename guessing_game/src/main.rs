664b3fb909b2c219cd01c23f683d8d8ff7e01664
Deepankar01 <deepankar01agrawal@gmail.com>
2019-05-07 15:01:47 +0530

    gueesing game completed with little knowledge

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,25 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Enter a number");
-    let mut guess = String::new(); //mutable
-    io::stdin().read_line(&mut guess).expect("Failed to read the line"); //&mut the reference is mutable
-    println!("You entered {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is: {}", secret_number);
+    loop{
+        let mut user_entered_number = String::new(); //mutable
+        println!("Enter a number");
+        io::stdin().read_line(&mut user_entered_number).expect("Failed to read the line"); //&mut the reference is mutable
+        let user_entered_number:u32 = user_entered_number.trim().parse().expect("Failed to parse string to number");
+    
+        //comparison method no clue till now how match works 
+        match user_entered_number.cmp(&secret_number){
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("They are equal!");
+                break;
+            },
+        }
+    }
+    
 }
